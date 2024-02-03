6b4a9c531586c70ba396f1a43058d9d8011fd558
Theo Kretz <e52004057@student.tuwien.ac.at>
2024-02-03 13:41:24 +0100

    added routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
+import {ContactComponent} from "./components/contact/contact.component";
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
+    {path: 'kontakt', component: ContactComponent},
 ];
