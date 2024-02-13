f3527ad81aff61b68e1e509dec5913831f27c099
Theo Kretz <e52004057@student.tuwien.ac.at>
2024-02-13 20:38:20 +0100

    added button with routing to tonys cafe

--- frontend/src/app/components/contact/contact.component.ts
+++ frontend/src/app/components/contact/contact.component.ts
@@ -33,53 +33,18 @@ export class ContactComponent {
 
     display: any;
     center: google.maps.LatLngLiteral = {
-        lat: 48.39406295120917,
-        lng: 15.577823410930153
+        lat: 48.39395386803803,
+        lng: 15.577857720236125
     };
     marker = {
-        position: { lat: 48.39406295120917, lng: 15.577823410930153 },
+        position: { lat:  48.39395386803803, lng: 15.577857720236125 },
     }
     zoom = 16;
 
-    /*------------------------------------------
-    --------------------------------------------
-    moveMap()
-    --------------------------------------------
-    --------------------------------------------*/
+
     moveMap(event: google.maps.MapMouseEvent) {
         if (event.latLng != null) this.center = (event.latLng.toJSON());
     }
-
-
-
-   async initMap(): Promise<void> {
-    // The location of Uluru
-    const position = { lat: -25.344, lng: 131.031 };
-
-    // Request needed libraries.
-    //@ts-ignore
-    const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
-    // @ts-ignore
-    const { AdvancedMarkerView } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
-
-    // The map, centered at Uluru
-    const map = new Map(
-        document.getElementById('map') as HTMLElement,
-        {
-          zoom: 4,
-          center: position,
-          mapId: 'DEMO_MAP_ID',
-        }
-    );
-
-    // The marker, positioned at Uluru
-    const marker = new AdvancedMarkerView({
-      map: map,
-      position: position,
-      title: 'Uluru'
-    });
-
-   }
 
   onSubmit(form: NgForm) {
     if (form.valid) {
