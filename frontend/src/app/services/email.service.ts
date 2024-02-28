18f0f7420dd32db0f66dc195e314da50b443a776
Theo Kretz <e52004057@student.tuwien.ac.at>
2024-02-28 16:13:43 +0100

    email sending now via SendGrid and AWS Lambda function, no backend needed anymore

--- frontend/src/app/services/email.service.ts
+++ frontend/src/app/services/email.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Email } from '../dtos/email';
 import { Observable } from 'rxjs';
-
+import { environment} from "../../environment/environment";
 import {Globals} from "../global/globals";
 
 @Injectable({
@@ -10,12 +10,21 @@ import {Globals} from "../global/globals";
 })
 export class EmailService {
   private emailBaseUri: string = this.globals.backendUri + '/kontakt/email';
+  private awsEndpoint: string = environment.awsEndpoint;
+
   constructor(private httpClient: HttpClient, private globals: Globals) { }
 
   sendEmail(email: Email): Observable<Email> {
+    return this.httpClient.post<Email>(this.awsEndpoint, email);
+  }
+
+  /*
+  Version used with Spring Boot backend
+  sendEmail(email: Email): Observable<Email> {
     return this.httpClient.post<Email>(
-      this.emailBaseUri,
-      email
+        this.emailBaseUri,
+        email
     );
   }
+  */
 }
